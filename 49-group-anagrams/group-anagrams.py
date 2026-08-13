408d732398a970c368c84175af33e9974859fa58
DARSHAN YARAGUPPI <darshanyaraguppi@gmail.com>
2026-08-13 16:27:15 +0530

    Time: 16 ms (88.84%) | Memory: 16.4 MB (33.33%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        group = {}
+
+        for word in strs:
+            key = "".join(sorted(word))
+
+            if key not in group:
+                group[key] = []
+        
+            group[key].append(word)
+
+        return list(group.values())
