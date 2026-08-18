f217b9ae853e0652eaf2e9b865b05167c3007f0c
DARSHAN YARAGUPPI <darshanyaraguppi@gmail.com>
2026-08-18 22:41:20 +0530

    Time: 22 ms (44.71%) | Memory: 12.4 MB (54.30%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -7,10 +7,10 @@ class Solution(object):
 
         dp = [0] * (n+1)
 
-        dp[0] = 0
-        dp[1] = 1
+        prev = 0
+        cur = 1
 
         for i in range(2,n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-
-        return dp[n]
+            prev,cur = cur,prev + cur
+            
+        return cur
